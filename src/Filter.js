e3780961d1beade3cc4b5c317060e7c96757bce0
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2024-04-05 17:18:00 +0300

    filter toggle logic

--- src/Filter.js
+++ src/Filter.js
@@ -1,24 +1,50 @@
+import {useState} from "react";
+
 export default function Filter({profi}){
+
+    const [filterToggle, setFilterToggle] = useState(false)
+    function toggleFilter() {
+        setFilterToggle(!filterToggle)
+    }
+
     const filterContainerStyle = {
-        color: profi.colors.accentColor
+        color: profi.colors.accentColor,
+        margin: 'auto',
+        width: '90%'
     }
     const switchStyle = {
         backgroundColor: profi.colors.secondaryColor,
-        padding: '10px',
-        display: 'inline-block'
+        padding: 10,
+        display: 'inline-block',
+        fontSize: 30,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        borderBottomLeftRadius: filterToggle?null:10,
+        borderBottomRightRadius: filterToggle?null:10
+
     };
     const settingsStyle = {
         backgroundColor: profi.colors.secondaryColor,
-        padding: '10px',
-        minHeight: '150px'
+        padding: 10,
+        minHeight: '150px',
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
+        borderTopRightRadius: 10
+
     };
 
     return <div className="Filter">
         <div className="filter-container" style={filterContainerStyle}>
-            <div className="switch" style={switchStyle}>Фильтр</div>
-            <div className="filter-settings" style={settingsStyle}>
-                Всякие настройки
+            <div className="switch" style={switchStyle} onClick={toggleFilter}>
+                <span>
+                    Фильтр
+                </span>
             </div>
+            {filterToggle ?
+                <div className="filter-settings" style={settingsStyle}>
+                    Всякие настройки
+                </div>
+            :null}
         </div>
     </div>
 }
